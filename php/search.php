d1c1096140984617e40ca238ba882518bc787227
Jose Israel Santizo Santos <joseisraelss.jiss@gmail.com>
2021-12-18 17:38:29 -0600

    Se modifica codigo para busqueda

--- php/search.php
+++ php/search.php
@@ -1,7 +1,13 @@
 <?php
-    include 'index.php';
+    include 'conection.php';
+    if(!isset($_POST['buscar'])){
+        $_POST['buscar'] = "";
+        $buscar = $_POST['buscar'];
+    }
     $buscar = $_POST['buscar'];
-    $SQL_READ = "SELECT * FROM catalogo WHERE nombre LIKE '%".$buscar."%'
-    OR editorial LIKE '%".$buscar."%'"; //Parametro de busqueda
+    $SQL_READ = "SELECT * FROM catalogo WHERE nombre LIKE '.$buscar.'
+    OR editorial LIKE '.$buscar.'"; //Parametro de busqueda
+    /*$SQL_READ = "SELECT * FROM catalogo WHERE nombre LIKE '%".$buscar."%'
+    OR editorial LIKE '%".$buscar."%'"; //Parametro de busqueda*/
     $sql_query = mysqli_query($conection, $SQL_READ);
 ?>
