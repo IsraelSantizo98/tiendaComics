da4e40e6742914f2dd0abcb6010f2b639083b249
Jose Israel Santizo Santos <joseisraelss.jiss@gmail.com>
2021-12-27 22:02:43 -0600

    Modificacion

--- php/conection.php
+++ php/conection.php
@@ -2,7 +2,7 @@
     $server  = "localhost";
     $user = "root";
     $password = "";
-    $database = "comics";
+    $database = "tiendaonline";
     $conection = mysqli_connect($server, $user, $password, $database);
     /*if($conection == true){
         echo "Conexion exitosa";
