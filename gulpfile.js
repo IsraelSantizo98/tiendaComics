fb63e8bfa29b63147fdcf77867d8537abb5fea58
Jose Israel Santizo Santos <joseisraelss.jiss@gmail.com>
2022-01-02 19:48:12 -0600

    Archivo con funciones de compilacion scss y webp

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+//CSS
+//Retorna funciones necesarias para el proyecto, son apis
+const {src, dest, watch, parallel} = require('gulp');
+//Indica la ruta para poder compilar sass y traducirlo en css y que el navegador lo puede reconocer
+const sass = require ("gulp-sass")(require('sass'));
+const plumber = require('gulp-plumber');
+//Imagenes
+const webp = require ('gulp-webp');
+function css (done){
+    //Se asigna la ruta donde esta el archivo a compilar y los asteriscos le dice que todos los archivos con esa extension
+    src('src/scss/**/*.scss')
+        //Evitar que se detenga el workflow con algun error
+        .pipe(plumber())
+        //Leer el archivo y compilarlo
+        .pipe(sass())
+        //Donde almacena el archivo compilado
+        .pipe(dest('build/css'))
+    //Finaliza la tarea    
+    done();
+}
+function versionWebp(done){
+    const opciones = {
+        //La calidad de las imagnes, de 1 a 100
+        quality: 100
+    };
+    //La ruta donde se guarda los archivos convertidos y que sean con la extensiones en parentesis
+    src('src/img/**/*.{png, jpg}')
+        //Se pasa el parametro de la calidad de las imagenes
+        .pipe(webp(opciones))
+        //Ruta de destino o guradado
+        .pipe(dest('build/img'))
+    //Finaliza la tarea
+    done();
+}
+function dev(done){
+    watch('src/scss/**/*.scss', css)
+    done();
+}
+//Habilitar las funciones y poder mandarlas a llamar
+exports.css = css;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
